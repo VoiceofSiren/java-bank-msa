6e98a46273961ae9dca4a756a97ad9aea88fe7bd
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-07 15:57:12 +0900

    first commit

---
+++ src/main/java/org/example/bankmsa/BankMsaApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankmsa/BankMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
