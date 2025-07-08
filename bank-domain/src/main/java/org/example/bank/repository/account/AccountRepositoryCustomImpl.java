5e2ca78cfccd27cb6a8e85646548cf760cf90302
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-08 18:44:25 +0900

    fix: change QClass settings

--- bank-domain/src/main/java/org/example/bank/repository/account/AccountRepositoryCustomImpl.java
+++ bank-domain/src/main/java/org/example/bank/repository/account/AccountRepositoryCustomImpl.java
@@ -3,6 +3,8 @@ package org.example.bank.repository.account;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.example.bank.entity.Account;
+import org.example.bank.entity.QAccount;;
+
 
 @RequiredArgsConstructor
 public class AccountRepositoryCustomImpl implements AccountRepositoryCustom{
@@ -11,6 +13,7 @@ public class AccountRepositoryCustomImpl implements AccountRepositoryCustom{
 
     @Override
     public Account findByAccountNumber(String accountNumber) {
+        QAccount qAccount = QAccount.account;
         return null;
     }
 }
