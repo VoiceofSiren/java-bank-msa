1fc304b41eab8d8ffd29e5c3416e39c41a8c247a
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-09 16:53:35 +0900

    feat: define queryDsl-based method for Account entity

--- bank-domain/src/main/java/org/example/bank/entity/Account.java
+++ bank-domain/src/main/java/org/example/bank/entity/Account.java
@@ -1,16 +1,25 @@
 package org.example.bank.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "accounts")
-public class Account {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id = 0L;
 
     @Column(nullable = false, unique = true)
@@ -24,4 +33,5 @@ public class Account {
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
 }
--- bank-domain/src/main/java/org/example/bank/repository/account/AccountRepositoryCustomImpl.java
+++ bank-domain/src/main/java/org/example/bank/repository/account/AccountRepositoryCustomImpl.java
@@ -1,5 +1,6 @@
 package org.example.bank.repository.account;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.example.bank.entity.Account;
@@ -14,6 +15,24 @@ public class AccountRepositoryCustomImpl implements AccountRepositoryCustom{
     @Override
     public Account findByAccountNumber(String accountNumber) {
         QAccount qAccount = QAccount.account;
-        return null;
+
+        Tuple tuple = queryFactory
+                .select(qAccount.id,
+                        qAccount.accountNumber,
+                        qAccount.balance,
+                        qAccount.accountHolderName,
+                        qAccount.createdAt
+                )
+                .from(qAccount)
+                .where(qAccount.accountNumber.eq(accountNumber))
+                .fetchOne();
+
+        return Account.builder()
+                .id(tuple.get(qAccount.id))
+                .accountNumber(tuple.get(qAccount.accountNumber))
+                .balance(tuple.get(qAccount.balance))
+                .accountHolderName(tuple.get(qAccount.accountHolderName))
+                .createdAt(tuple.get(qAccount.createdAt))
+                .build();
     }
 }
