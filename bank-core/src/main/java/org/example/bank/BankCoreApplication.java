819753a586a15c30d527dc66540837c4a6aa1fc2
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-10 15:37:52 +0900

    feat: create bank-core module and add dependencies between modules

---
+++ bank-core/src/main/java/org/example/bank/BankCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCoreApplication.class, args);
+    }
+
+}
