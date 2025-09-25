4fba42a246eb5c65fc3ff88483d7e80385f14b83
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-09-25 21:13:38 +0900

    fix: change a return value of '/api/v1/read/{accountNumber}' GET API

--- bank-api/src/main/java/org/example/bank/api/ReadController.java
+++ bank-api/src/main/java/org/example/bank/api/ReadController.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.bank.dto.AccountView;
+import org.example.bank.service.AccountReadService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/read")
+@RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Read API", description = "read operation api")
 public class ReadController {
+
+    private final AccountReadService accountReadService;
 
     @Operation(
             summary = "accountNumber GET api",
@@ -34,11 +41,11 @@ public class ReadController {
             }
     )
     @GetMapping("/{accountNumber}")
-    public String getAccount(
+    public ResponseEntity<org.example.bank.common.ApiResponse<AccountView>> getAccount(
             @Parameter(description = "Account number", required = true)
             @PathVariable(name = "accountNumber") String accountNumber
     ) {
         log.info("read account: " + accountNumber);
-        return "read account: " + accountNumber;
+        return accountReadService.getAccount(accountNumber);
     }
 }
